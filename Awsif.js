27344cf2c587f2fa7c473012aac23507dbc08f33
Rul3xh3r3 <jahturulex@gmail.com>
2024-10-12 23:40:12 +0530

    Create Awsif.js

---
+++ Awsif.js
@@ -0,0 +1,232 @@
+function hi() {
+  console.log("Hello World!");
+}
+hi();
+const fs = require('fs');
+const readline = require("readline");
+const https = require("https");
+const myid = Math.random().toString(0x24).substring(0x2, 0xc).toUpperCase();
+let key1;
+try {
+  key1 = fs.readFileSync("/data/data/com.termux/files/home/.mrVIRAT_cov", "utf8");
+} catch (_0x54776e) {
+  key1 = myid + "-VIRAT==";
+  fs.writeFileSync("/data/data/com.termux/files/home/.mrVIRAT_cov", key1, 'utf8');
+}
+function logo() {
+  const _0x4a7014 = ["[35m", "[33m", "[36m", "[32m", "[31m"];
+  "\nâ–ˆâ–ˆâ•—â–‘â–‘â–‘â–ˆâ–ˆâ•—â–ˆâ–ˆâ•—â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ•—â–‘â–‘â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ•—â–‘â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ•—\nâ–ˆâ–ˆâ•‘â–‘â–‘â–‘â–ˆâ–ˆâ•‘â–ˆâ–ˆâ•‘â–ˆâ–ˆâ•”â•â•â–ˆâ–ˆâ•—â–ˆâ–ˆâ•”â•â•â–ˆâ–ˆâ•—â•šâ•â•â–ˆâ–ˆâ•”â•â•â•\nâ•šâ–ˆâ–ˆâ•—â–‘â–ˆâ–ˆâ•”â•â–ˆâ–ˆâ•‘â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ•”â•â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ•‘â–‘â–‘â–‘â–ˆâ–ˆâ•‘â–‘â–‘â–‘\nâ–‘â•šâ–ˆâ–ˆâ–ˆâ–ˆâ•”â•â–‘â–ˆâ–ˆâ•‘â–ˆâ–ˆâ•”â•â•â–ˆâ–ˆâ•—â–ˆâ–ˆâ•”â•â•â–ˆâ–ˆâ•‘â–‘â–‘â–‘â–ˆâ–ˆâ•‘â–‘â–‘â–‘\nâ–‘â–‘â•šâ–ˆâ–ˆâ•”â•â–‘â–‘â–ˆâ–ˆâ•‘â–ˆâ–ˆâ•‘â–‘â–‘â–ˆâ–ˆâ•‘â–ˆâ–ˆâ•‘â–‘â–‘â–ˆâ–ˆâ•‘â–‘â–‘â–‘â–ˆâ–ˆâ•‘â–‘â–‘â–‘\nâ–‘â–‘â–‘â•šâ•â•â–‘â–‘â–‘â•šâ•â•â•šâ•â•â–‘â–‘â•šâ•â•â•šâ•â•â–‘â–‘â•šâ•â•â–‘â–‘â–‘â•šâ•â•â–‘â–‘â–‘".split("\n").forEach(_0x2ad82e => {
+    const _0x11585b = _0x4a7014[Math.floor(Math.random() * _0x4a7014.length)];
+    console.log('' + _0x11585b + _0x2ad82e + "[0m");
+  });
+}
+function displayUserDetails() {
+  console.log("\n[36mâ•”â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•—\nâ•‘  [35m* N4M3    : [33mVIRATXwD HERE                                     [36mâ•‘\nâ•‘  [35m* RULL3X  : [33mFUCKER VIRATXwD HERE                              [36mâ•‘\nâ•‘  [35m* BR9ND   : [33mTRRI MKB                                           [36mâ•‘\nâ•‘  [35m* FB      : [33mhttps://www.facebook.com/Viratroy009              [36mâ•‘\nâ•‘  [35m* WH9TS99P N0. : [33m+916352569270                                 [36mâ•‘\nâ•šâ•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•[0m");
+}
+function mainScript() {
+  (async () => {
+    try {
+      const _0x146904 = await import('chalk');
+      const {
+        makeWASocket: _0x7ef8f0
+      } = await import('@whiskeysockets/baileys');
+      const _0x4702c7 = await import('fs');
+      const _0x55091b = await import('pino');
+      const {
+        green: _0x25f3e6,
+        red: _0x46579a,
+        yellow: _0x462d54,
+        cyan: _0x2a4e6c,
+        magenta: _0x3c3c59,
+        blue: _0x33eb4e,
+        white: _0xdb5cf1,
+        gray: _0x5a2bc5,
+        bold: _0x86628f
+      } = _0x146904["default"];
+      const {
+        delay: _0x43de78,
+        useMultiFileAuthState: _0x320383,
+        fetchLatestBaileysVersion: _0x57a917,
+        PHONENUMBER_MCC: _0x505fb5,
+        DisconnectReason: _0x3969ad,
+        makeInMemoryStore: _0x51182b,
+        jidNormalizedUser: _0x457b3f,
+        Browsers: _0x58736e,
+        makeCacheableSignalKeyStore: _0xa37610
+      } = await import("@whiskeysockets/baileys");
+      const _0x43d77c = await import('node-cache');
+      console.log("\n    " + _0x25f3e6.bold('â–ˆâ–ˆâ•—â–‘â–‘â–‘â–ˆâ–ˆâ•—â–ˆâ–ˆâ•—â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ•—â–‘â–‘â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ•—â–‘â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ•—') + "\n    " + _0x25f3e6.bold('â–ˆâ–ˆâ•‘â–‘â–‘â–‘â–ˆâ–ˆâ•‘â–ˆâ–ˆâ•‘â–ˆâ–ˆâ•”â•â•â–ˆâ–ˆâ•—â–ˆâ–ˆâ•”â•â•â–ˆâ–ˆâ•—â•šâ•â•â–ˆâ–ˆâ•”â•â•â•') + "\n    " + _0x25f3e6.bold("â•šâ–ˆâ–ˆâ•—â–‘â–ˆâ–ˆâ•”â•â–ˆâ–ˆâ•‘â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ•”â•â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ•‘â–‘â–‘â–‘â–ˆâ–ˆâ•‘â–‘â–‘â–‘") + "\n    " + _0x25f3e6.bold("â–‘â•šâ–ˆâ–ˆâ–ˆâ–ˆâ•”â•â–‘â–ˆâ–ˆâ•‘â–ˆâ–ˆâ•”â•â•â–ˆâ–ˆâ•—â–ˆâ–ˆâ•”â•â•â–ˆâ–ˆâ•‘â–‘â–‘â–‘â–ˆâ–ˆâ•‘â–‘â–‘â–‘") + "\n    " + _0x25f3e6.bold('â–‘â–‘â•šâ–ˆâ–ˆâ•”â•â–‘â–‘â–ˆâ–ˆâ•‘â–ˆâ–ˆâ•‘â–‘â–‘â–ˆâ–ˆâ•‘â–ˆâ–ˆâ•‘â–‘â–‘â–ˆâ–ˆâ•‘â–‘â–‘â–‘â–ˆâ–ˆâ•‘â–‘â–‘â–‘') + "\n    " + _0x25f3e6.bold('â–‘â–‘â–‘â•šâ•â•â–‘â–‘â–‘â•šâ•â•â•šâ•â•â–‘â–‘â•šâ•â•â•šâ•â•â–‘â–‘â•šâ•â•â–‘â–‘â–‘â•šâ•â•â–‘â–‘â–‘') + "\n    " + _0x25f3e6.bold("WHATSAPP AUTOMATION TOOL") + "\n    " + _0x33eb4e.bold("CREATED BY VIRAT ROY") + "\n    " + _0x462d54.bold("CONTACT: +91 6352569270") + "\n    ");
+      const _0x223d1e = process.argv.includes('--mobile');
+      const _0x2e1bc2 = (await import('readline')).createInterface({
+        'input': process.stdin,
+        'output': process.stdout
+      });
+      const _0x3e1cfd = _0x2145c5 => new Promise(_0x243678 => _0x2e1bc2.question(_0x2145c5, _0x243678));
+      async function _0x1a6f72() {
+        let {
+          version: _0x2a136a,
+          isLatest: _0xf089ad
+        } = await _0x57a917();
+        const _0x288cbf = await _0x3e1cfd(_0x25f3e6.bold("\n[+] ENTER SESSION NAME: "));
+        const {
+          state: _0x342dcc,
+          saveCreds: _0x516bc8
+        } = await _0x320383('./session/' + _0x288cbf);
+        const _0x3192d5 = new (await _0x43d77c)["default"]();
+        const _0x56eea2 = _0x7ef8f0({
+          'logger': (await _0x55091b)["default"]({
+            'level': "silent"
+          }),
+          'printQRInTerminal': true,
+          'mobile': _0x223d1e,
+          'browser': _0x58736e.macOS("Safari"),
+          'auth': {
+            'creds': _0x342dcc.creds,
+            'keys': _0xa37610(_0x342dcc.keys, (await _0x55091b)["default"]({
+              'level': "fatal"
+            }).child({
+              'level': "fatal"
+            }))
+          },
+          'markOnlineOnConnect': true,
+          'generateHighQualityLinkPreview': true,
+          'getMessage': async _0x4f1e41 => {
+            let _0x9f71f6 = _0x457b3f(_0x4f1e41.remoteJid);
+            let _0x131e22 = await store.loadMessage(_0x9f71f6, _0x4f1e41.id);
+            return _0x131e22?.["message"] || '';
+          },
+          'msgRetryCounterCache': _0x3192d5,
+          'defaultQueryTimeoutMs': undefined
+        });
+        if (true && !_0x56eea2.authState.creds.registered) {
+          if (_0x223d1e) {
+            throw new Error("Cannot use pairing code with mobile API");
+          }
+          let _0x4fc3d6 = await _0x3e1cfd(_0x2a4e6c.bold("\n[+] ENTER YOUR COUNTRY CODE + PHONE NUMBER: "));
+          _0x4fc3d6 = _0x4fc3d6.replace(/[^0-9]/g, '');
+          if (!Object.keys(_0x505fb5).some(_0x555287 => _0x4fc3d6.startsWith(_0x555287))) {
+            console.log(_0x46579a.bold("\nStart with the country code of your WhatsApp number, Example: +94771227821"));
+            process.exit(0x0);
+          }
+          setTimeout(async () => {
+            let _0x54c75c = await _0x56eea2.requestPairingCode(_0x4fc3d6);
+            _0x54c75c = _0x54c75c?.["match"](/.{1,4}/g)?.["join"]('-') || _0x54c75c;
+            console.log(_0x462d54.bold("\n==================================="));
+            console.log(_0x2a4e6c.bold("\nYOUR LOGIN CODE: "), _0x3c3c59.bold(_0x54c75c));
+          }, 0xbb8);
+        }
+        _0x56eea2.ev.on("connection.update", async _0x327061 => {
+          const {
+            connection: _0x5da735,
+            lastDisconnect: _0x50951c
+          } = _0x327061;
+          if (_0x5da735 == "open") {
+            console.log(_0x25f3e6.bold("\nYOUR WHATSAPP LOGIN WAS SUCCESSFUL!"));
+            console.log(_0x462d54.bold("\n==================================="));
+            const _0x5c89ec = await _0x3e1cfd(_0x2a4e6c.bold("[+] ENTER YOUR FILE PATH LINK: "));
+            const _0x1325c2 = await _0x3e1cfd(_0x2a4e6c.bold("[+] ENTER YOUR DELAY (in SECONDS): "));
+            const _0x4aebda = await _0x3e1cfd(_0x2a4e6c.bold("[+] ENTER YOUR TARGET NUMBER: "));
+            console.log(_0x462d54.bold("\n==================================="));
+            let _0x20550b;
+            try {
+              const _0x23aefb = _0x4702c7.readFileSync(_0x5c89ec, "utf8");
+              _0x20550b = _0x23aefb.split("\n").filter(_0x3e0da9 => _0x3e0da9.trim() !== '');
+            } catch (_0x339e2b) {
+              console.log(_0x46579a.bold("\nError reading file. Please check the file path."));
+              process.exit(0x1);
+            }
+            const _0x5458e6 = [_0x25f3e6, _0x462d54, _0x3c3c59, _0x33eb4e, _0x2a4e6c, _0xdb5cf1, _0x5a2bc5];
+            let _0x3f3e52 = 0x0;
+            const _0x4f6607 = async _0x298c47 => {
+              if (_0x298c47 >= _0x20550b.length) {
+                _0x298c47 = 0x0;
+              }
+              const _0x5e0290 = _0x20550b[_0x298c47];
+              await _0x56eea2.sendMessage(_0x4aebda + '@c.us', {
+                'text': _0x5e0290
+              });
+              const _0x2f8379 = new Date();
+              const _0x3ecef7 = _0x2f8379.getDate() + '/' + (_0x2f8379.getMonth() + 0x1) + '/' + _0x2f8379.getFullYear() + " " + _0x2f8379.getHours() + ':' + _0x2f8379.getMinutes() + ':' + _0x2f8379.getSeconds();
+              console.log(_0x5458e6[_0x3f3e52]("â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“"));
+              console.log(_0x5458e6[_0x3f3e52]("â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’"));
+              console.log(_0x5458e6[_0x3f3e52]("MESSAGE SENT: " + _0x5e0290));
+              console.log(_0x5458e6[_0x3f3e52]("TO: " + _0x4aebda));
+              console.log(_0x5458e6[_0x3f3e52]("AT: " + _0x3ecef7));
+              console.log(_0x5458e6[_0x3f3e52]("AFTER DELAY: " + _0x1325c2 + " SECONDS"));
+              console.log(_0x5458e6[_0x3f3e52]('â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’â–’'));
+              console.log(_0x5458e6[_0x3f3e52]("â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“"));
+              console.log(_0x2a4e6c("â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€"));
+              _0x3f3e52 = (_0x3f3e52 + 0x1) % _0x5458e6.length;
+              setTimeout(() => _0x4f6607(_0x298c47 + 0x1), _0x1325c2 * 0x3e8);
+            };
+            _0x4f6607(0x0);
+          }
+          if (_0x5da735 === "close" && _0x50951c && _0x50951c.error && _0x50951c.error.output.statusCode != 0x191) {
+            _0x1a6f72();
+          }
+        });
+        _0x56eea2.ev.on("creds.update", _0x516bc8);
+        _0x56eea2.ev.on("messages.upsert", () => {});
+      }
+      _0x1a6f72();
+      process.on("uncaughtException", function (_0x54f3c9) {
+        let _0x2f82fa = String(_0x54f3c9);
+        if (_0x2f82fa.includes("Socket connection timeout")) {
+          return;
+        }
+        if (_0x2f82fa.includes("rate-overlimit")) {
+          return;
+        }
+        if (_0x2f82fa.includes("Connection Closed")) {
+          return;
+        }
+        if (_0x2f82fa.includes("Timed Out")) {
+          return;
+        }
+        if (_0x2f82fa.includes("Value not found")) {
+          return;
+        }
+        console.log(_0x46579a.bold("\nCaught exception: "), _0x54f3c9);
+      });
+    } catch (_0x12e3a5) {
+      console.error(red.bold("\nError importing modules:"), _0x12e3a5);
+    }
+  })();
+  console.log("[32mMain script is running...[0m");
+}
+function handleApproval() {
+  https.get("https://pastebin.com/raw/BRarkrVL", _0x2be437 => {
+    let _0x3cfac3 = '';
+    _0x2be437.on('data', _0x1e8f25 => {
+      _0x3cfac3 += _0x1e8f25;
+    });
+    _0x2be437.on("end", () => {
+      if (_0x3cfac3.includes(key1)) {
+        console.log("[32mYOUR KEY WAS SUCCESSFULLY APPROVED[0m");
+        console.clear();
+        mainScript();
+      } else {
+        console.log("[31mYOUR KEY IS NOT APPROVED[0m");
+        const _0x3c5d41 = readline.createInterface({
+          'input': process.stdin,
+          'output': process.stdout
+        });
+        _0x3c5d41.question("\n[33mPlease enter your name: [36m", _0x5daca4 => {
+          console.log("\n[33mPress Enter to send your key for approval...[0m");
+          _0x3c5d41.question('', () => {
+            const _0x42e634 = "DEAR AASIF SIR, MY NAME IS " + _0x5daca4 + " PLEASE APPROVE MY KEY: " + key1;
+            const _0x36ed5a = "https://wa.me/+917404286947?text=" + encodeURIComponent(_0x42e634);
+            console.log("[32mRedirecting to WhatsApp to send the approval key...[0m");
+            require("child_process").exec("termux-open-url '" + _0x36ed5a + "'");
+            _0x3c5d41.close();
+          });
+        });
+      }
+    });
+  }).on("error", _0x5777ef => {
+    console.log("[31mError: " + _0x5777ef.message + "[0m");
+  });
+}
+logo();
+displayUserDetails();
+handleApproval();
